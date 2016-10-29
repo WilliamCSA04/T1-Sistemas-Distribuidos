74543b670c993eb9e123253bd2aa82233ae74329
williamcsa04 <williamcaetano14@gmail.com>
2016-10-29 14:55:29 -0200

    Added more methods to Board class

--- src/Speculate/Board.java
+++ src/Speculate/Board.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Speculate;
 
-/**
- *
- * @author realnetwoking
- */
 public class Board {
+    
+    boolean spaces[];
+    
+    public Board(){
+        populateInitialSpaces();
+    }
+    
+    private void populateInitialSpaces(){
+        spaces = new boolean[]{true, false, true, false, true};
+    }
+    
+    
+    public boolean playResult(int space){
+        isAValueArgument(space);
+        boolean actualValue =  spaces[space];
+        spaces[space] = !actualValue;
+        return actualValue;
+    }
+    
+    private void isAValueArgument(int argument){
+        boolean isOutOfRange = argument>4 || argument<0;
+        if(isOutOfRange){
+            throw new IllegalArgumentException();
+        }
+    }
     
 }
