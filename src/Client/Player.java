0670a1951b3bf90469e6bcfde5c5b80b93013651
williamcsa04 <williamcaetano14@gmail.com>
2016-10-29 16:35:18 -0200

    Added client player

---
+++ src/Client/Player.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author realnetwoking
+ */
+public class Player {
+    
+    String name;
+    
+    
+    public Player(String name){
+        this.name = name;
+    }
+    
+}
