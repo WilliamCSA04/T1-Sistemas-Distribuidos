1361dfb05c78c583005cb8611f98e67ec5bbbca8
williamcsa04 <williamcaetano14@gmail.com>
2016-10-29 19:26:32 -0200

    Created code to test game without rmi

--- src/Initializer/Application.java
+++ src/Initializer/Application.java
@@ -1,12 +1,18 @@
 package Initializer;
 
-import Speculate.Dice;
+import Client.Player;
+import Server.Game;
 
 public class Application {
     
     public static void main(String[] args) {
-
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
         
+        Game game = new Game(player1, player2);
+        while(game.play(game.getPlayerThatHasToPlay(), game.getPlayerThatHasToPlay().getActualBallsQuantity()) != 1){
+            System.out.println("name: " + game.getPlayerThatHasToPlay().getName() + ", balls: " + game.getPlayerThatHasToPlay().getActualBallsQuantity());
+        }
     }
     
 }
